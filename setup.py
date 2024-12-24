7731447364e1b5becc33db772a263f356b36b3b0
Rosh J <AroshaJ@spectral-solutions.com>
2024-12-24 14:58:56 +1300

    Updated requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 from setuptools import setup, find_packages
 
-# Read requirements from requirements.txt
-def read_requirements():
-    with open("requirements.txt", "r") as f:
-        return f.read().splitlines()
-
 setup(
-    name="python-analyst-utility", 
+    name="python-analyst-utility",
     version="0.1.0",
     author="Rosh Jayawardena",
     author_email="rosh.jayawardena@gmail.com",
@@ -21,6 +16,32 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
-    install_requires=read_requirements(),  # Dynamically load from requirements.txt
+    install_requires=[
+        "certifi>=2024.12.14",
+        "charset-normalizer>=3.4.0",
+        "et_xmlfile>=2.0.0",
+        "idna>=3.10",
+        "numpy>=2.2.1",
+        "openpyxl>=3.1.5",
+        "packaging>=24.2",
+        "pandas>=2.2.3",
+        "python-dateutil>=2.9.0.post0",
+        "pytz>=2024.2",
+        "requests>=2.32.3",
+        "urllib3>=2.3.0",
+        "xlwings>=0.33.5",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=8.3.4",
+            "pytest-mock>=3.14.0",
+            "twine>=6.0.1",
+            "readme_renderer>=44.0",
+            "pkginfo>=1.12.0",
+            "keyring>=25.5.0",
+            "rich>=13.9.4",
+        ],
+    },
 )
+
 
